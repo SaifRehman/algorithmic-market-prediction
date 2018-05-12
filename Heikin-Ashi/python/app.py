d96f7775e19a68fb35fa05c40d1ef6e46c5dfad2
saifrehman <saifrehman@saifrehmans-MacBook-Pro.local>
2018-05-13 00:47:43 +0400

    ready

--- Heikin-Ashi/python/app.py
+++ Heikin-Ashi/python/app.py
@@ -1,15 +1,18 @@
 import requests
 import json
 from twilio.rest import Client
-import datetime, timedelta
+from datetime import datetime, timedelta
 from flask import Flask, request
 import schedule
 import time
 from threading import Thread
 app = Flask(__name__)
 global HA_Close_latest 
+HA_Close_latest = 0 
 global HA_Open_latest 
+HA_Open_latest = 0
 global temp
+temp = 0
 def sms():
     print("Running periodic task!")
     utc_datetime_current = datetime.datetime.utcnow()
@@ -26,26 +29,27 @@ def sms():
         latest = response.content
         latest = json.loads(latest)
         latest = latest[0]
-        if(HA_Close_latest == 0):  
-            HA_Open_latest = (1320.978+1319.854) /2
-        else:
-            HA_Open_latest = (HA_Open_latest+HA_Close_latest)/2
-        HA_Close_latest = (latest["Open"] + latest["Close"] +  latest["Low"] + latest["High"]) /4
-        account_sid = 'ACe4f8a304ab7204bd3adb16decbf35e28'
-        auth_token = '7eaaeaac35ee2382a51906c6684a03a0'
-        client = Client(account_sid, auth_token)
-        if (HA_Open_latest < HA_Close_latest):
-            message = client.messages.create(
-                                    body="Buy",
-                                    from_="+13612382106",
-                                    to="+971567515767"
-                                )
-        else:
-            message = client.messages.create(
-                                    body="Sell",
-                                    from_="+13612382106",
-                                    to="+971567515767"
-                                )
+        if(latest["Open"]!=0 and latest["Close"]!=0 and latest["Low"]!=0 and latest["High"]!=0 ):
+            if(HA_Close_latest == 0):  
+                HA_Open_latest = (1320.978+1319.854) /2
+            else:
+                HA_Open_latest = (HA_Open_latest+HA_Close_latest)/2
+            HA_Close_latest = (latest["Open"] + latest["Close"] +  latest["Low"] + latest["High"]) /4
+            account_sid = 'ACe4f8a304ab7204bd3adb16decbf35e28'
+            auth_token = '7eaaeaac35ee2382a51906c6684a03a0'
+            client = Client(account_sid, auth_token)
+            if (HA_Open_latest < HA_Close_latest):
+                message = client.messages.create(
+                                        body="Buy",
+                                        from_="+13612382106",
+                                        to="+971567515767"
+                                    )
+            else:
+                message = client.messages.create(
+                                        body="Sell",
+                                        from_="+13612382106",
+                                        to="+971567515767"
+                                    )
 
 def run_schedule():
     while 1:
@@ -53,18 +57,18 @@ def run_schedule():
         time.sleep(1)  
 
 if __name__ == '__main__':
-    schedule.every().day.at("01:00 UTC").do(sms)
-    schedule.every().day.at("03:00 UTC").do(sms)
-    schedule.every().day.at("05:00 UTC").do(sms)
-    schedule.every().day.at("07:00 UTC").do(sms)
-    schedule.every().day.at("09:00 UTC").do(sms)
-    schedule.every().day.at("11:00 UTC").do(sms)
-    schedule.every().day.at("13:00 UTC").do(sms)
-    schedule.every().day.at("15:00 UTC").do(sms)
-    schedule.every().day.at("17:00 UTC").do(sms)
-    schedule.every().day.at("19:00 UTC").do(sms)
-    schedule.every().day.at("21:00 UTC").do(sms)
-    schedule.every().day.at("23:00 UTC").do(sms)
+    schedule.every().day.at("01:00").do(sms)
+    schedule.every().day.at("03:00").do(sms)
+    schedule.every().day.at("05:00").do(sms)
+    schedule.every().day.at("07:00").do(sms)
+    schedule.every().day.at("09:00").do(sms)
+    schedule.every().day.at("11:00").do(sms)
+    schedule.every().day.at("13:00").do(sms)
+    schedule.every().day.at("15:00").do(sms)
+    schedule.every().day.at("17:00").do(sms)
+    schedule.every().day.at("19:00").do(sms)
+    schedule.every().day.at("21:00").do(sms)
+    schedule.every().day.at("23:00").do(sms)
     t = Thread(target=run_schedule)
     t.start()
     app.run(host='0.0.0.0', port=5000)
